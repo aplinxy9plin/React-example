f7f82d50ec8d8e8223139d3591b344b2a80b48dc
Nikita Aplin <aplinxy9plin@icloud.com>
2018-03-10 09:00:40 +0700

    flex
    Flex fill some piece of screen. flex: 1 - full height

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -3,19 +3,50 @@ import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native';
 import { Button } from 'react-native';
 
-export default class App extends React.Component {
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      console.log('test');
+      <Text>{display}</Text>
     );
   }
 }
 
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: '20px',
   },
 });
